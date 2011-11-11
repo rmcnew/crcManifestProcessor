19e8433709b978a8e0c43204e797674f553314d2
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2011-11-11 18:04:14 +0300

    Add model for Excel cells

--- src/net/mcnewfamily/rmcnew/model/excel/CellEssence.java
+++ src/net/mcnewfamily/rmcnew/model/excel/CellEssence.java
@@ -19,6 +19,10 @@
 
 package net.mcnewfamily.rmcnew.model.excel;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+
 // contents and formatting data that will be used to
 // create a XSSFCell that is part of a spreadsheet
 // these cells have not yet been assigned a location within
@@ -27,4 +31,66 @@ public class CellEssence {
 
     // we mirror some parts of the POI Cell interface that are relevant to
     // the part of the cell we need to model
+    public enum CellType {
+        CELL_TYPE_NUMERIC (Cell.CELL_TYPE_NUMERIC),
+        CELL_TYPE_STRING (Cell.CELL_TYPE_STRING),
+        CELL_TYPE_FORMULA (Cell.CELL_TYPE_FORMULA),
+        CELL_TYPE_BLANK (Cell.CELL_TYPE_BLANK),
+        CELL_TYPE_BOOLEAN (Cell.CELL_TYPE_BOOLEAN),
+        CELL_TYPE_ERROR (Cell.CELL_TYPE_ERROR);
+
+        private int enumInt;
+
+        CellType(int enumInt) {
+            this.enumInt = enumInt;
+        }
+
+        public int toPoiCellType() {
+            return this.enumInt;
+
+        }
+    }
+
+    private CellType cellType = CellType.CELL_TYPE_STRING; // default to string CellType
+    private CellStyleEssence cellStyleEssence = null;
+    private String value;
+
+    public CellEssence() {
+    }
+
+    public CellType getCellType() {
+        return cellType;
+    }
+
+    public void setCellType(CellType cellType) {
+        this.cellType = cellType;
+    }
+
+    public CellStyleEssence getCellStyleEssence() {
+        return cellStyleEssence;
+    }
+
+    public void setCellStyleEssence(CellStyleEssence cellStyleEssence) {
+        this.cellStyleEssence = cellStyleEssence;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public XSSFCell toXSSFCell(XSSFRow row, int columnIndex) {
+        if (row != null && columnIndex >= 0) {
+            XSSFCell cell = row.createCell(columnIndex);
+            cell.setCellType(this.cellType.toPoiCellType());
+            cell.setCellValue(this.value);
+            cell.setCellStyle(this.cellStyleEssence.toXSSFCellStyle());
+            return cell;
+        } else {
+            throw new IllegalArgumentException("row cannot be null and columnIndex must be non-negative!");
+        }
+    }
 }
--- src/net/mcnewfamily/rmcnew/model/excel/CellStyleEssence.java
+++ src/net/mcnewfamily/rmcnew/model/excel/CellStyleEssence.java
@@ -19,9 +19,236 @@
 
 package net.mcnewfamily.rmcnew.model.excel;
 
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.awt.*;
+
 // formatting data that is used to style a CellEssence
 public class CellStyleEssence {
+// TODO:  add support for border colors and hiding
 
     // we mirror parts of the POI CellStyle interface that
     // are relevant to the cell style we need to model
+    public enum CellBorder {
+        BORDER_DASH_DOT (CellStyle.BORDER_DASH_DOT),
+        BORDER_DASH_DOT_DOT (CellStyle.BORDER_DASH_DOT_DOT),
+        BORDER_DASHED (CellStyle.BORDER_DASHED),
+        BORDER_DOTTED (CellStyle.BORDER_DOTTED),
+        BORDER_DOUBLE (CellStyle.BORDER_DOUBLE),
+        BORDER_HAIR (CellStyle.BORDER_HAIR),
+        BORDER_MEDIUM (CellStyle.BORDER_MEDIUM),
+        BORDER_MEDIUM_DASH_DOT (CellStyle.BORDER_DASH_DOT),
+        BORDER_MEDIUM_DASH_DOT_DOT (CellStyle.BORDER_DASH_DOT_DOT),
+        BORDER_MEDIUM_DASHED (CellStyle.BORDER_MEDIUM_DASHED),
+        BORDER_NONE (CellStyle.BORDER_NONE),
+        BORDER_SLANTED_DASH_DOT (CellStyle.BORDER_SLANTED_DASH_DOT),
+        BORDER_THICK (CellStyle.BORDER_THICK),
+        BORDER_THIN (CellStyle.BORDER_THIN);
+
+        private short enumShort;
+
+        CellBorder(short enumShort) {
+            this.enumShort = enumShort;
+        }
+
+        public short toPoiCellStyle() {
+            return this.enumShort;
+        }
+    }
+
+    public enum CellHorizontalAlignment {
+        ALIGN_CENTER (CellStyle.ALIGN_CENTER),
+        ALIGN_CENTER_SELECTION (CellStyle.ALIGN_CENTER_SELECTION),
+        ALIGN_FILL (CellStyle.ALIGN_FILL),
+        ALIGN_GENERAL (CellStyle.ALIGN_GENERAL),
+        ALIGN_JUSTIFY (CellStyle.ALIGN_JUSTIFY),
+        ALIGN_LEFT (CellStyle.ALIGN_LEFT),
+        ALIGN_RIGHT (CellStyle.ALIGN_RIGHT);
+
+        private short enumShort;
+
+        CellHorizontalAlignment(short enumShort) {
+            this.enumShort = enumShort;
+        }
+
+        public short toPoiCellStyle() {
+            return this.enumShort;
+        }
+    }
+
+    public enum CellVerticalAlignment {
+        VERTICAL_BOTTOM (CellStyle.VERTICAL_BOTTOM),
+        VERTICAL_CENTER (CellStyle.VERTICAL_CENTER),
+        VERTICAL_JUSTIFY (CellStyle.VERTICAL_JUSTIFY),
+        VERTICAL_TOP (CellStyle.VERTICAL_TOP);
+
+        private short enumShort;
+
+        CellVerticalAlignment(short enumShort) {
+            this.enumShort = enumShort;
+        }
+
+        public short toPoiCellStyle() {
+            return this.enumShort;
+        }
+    }
+
+    public enum CellFillPattern {
+        ALT_BARS (CellStyle.ALT_BARS),
+        BIG_SPOTS (CellStyle.BIG_SPOTS),
+        BRICKS (CellStyle.BRICKS),
+        DIAMONDS (CellStyle.DIAMONDS),
+        FINE_DOTS (CellStyle.FINE_DOTS),
+        LEAST_DOTS (CellStyle.LEAST_DOTS),
+        LESS_DOTS (CellStyle.LESS_DOTS),
+        NO_FILL (CellStyle.NO_FILL),
+        SOLID_FOREGROUND (CellStyle.SOLID_FOREGROUND),
+        SPARSE_DOTS (CellStyle.SPARSE_DOTS),
+        SQUARES (CellStyle.SQUARES),
+        THICK_BACKWARD_DIAG (CellStyle.THICK_BACKWARD_DIAG),
+        THICK_FORWARD_DIAG (CellStyle.THICK_FORWARD_DIAG),
+        THICK_HORZ_BANDS (CellStyle.THICK_HORZ_BANDS),
+        THICK_VERT_BANDS (CellStyle.THICK_VERT_BANDS),
+        THIN_BACKWARD_DIAG (CellStyle.THICK_BACKWARD_DIAG),
+        THIN_FORWARD_DIAG (CellStyle.THICK_FORWARD_DIAG),
+        THIN_HORZ_BANDS (CellStyle.THICK_HORZ_BANDS),
+        THIN_VERT_BANDS (CellStyle.THIN_VERT_BANDS);
+
+        private short enumShort;
+
+        CellFillPattern(short enumShort) {
+            this.enumShort = enumShort;
+        }
+
+        public short toPoiCellStyle() {
+            return this.enumShort;
+        }
+    }
+
+    // defaults
+    private CellBorder topBorder = CellBorder.BORDER_NONE;
+    private CellBorder bottomBorder = CellBorder.BORDER_NONE;
+    private CellBorder leftBorder = CellBorder.BORDER_NONE;
+    private CellBorder rightBorder = CellBorder.BORDER_NONE;
+    private CellVerticalAlignment verticalAlignment = CellVerticalAlignment.VERTICAL_BOTTOM;
+    private CellHorizontalAlignment horizontalAlignment = CellHorizontalAlignment.ALIGN_LEFT;
+    private CellFillPattern fillPattern = CellFillPattern.NO_FILL;
+    private XSSFColor foregroundColor = new XSSFColor(Color.BLACK);
+    private XSSFColor backgroundColor = new XSSFColor(Color.WHITE);
+    private boolean wrappedText = false;
+    private short indentationInSpaces = 0;
+
+    public CellStyleEssence() {
+    }
+
+    public CellBorder getTopBorder() {
+        return topBorder;
+    }
+
+    public void setTopBorder(CellBorder topBorder) {
+        this.topBorder = topBorder;
+    }
+
+    public CellBorder getBottomBorder() {
+        return bottomBorder;
+    }
+
+    public void setBottomBorder(CellBorder bottomBorder) {
+        this.bottomBorder = bottomBorder;
+    }
+
+    public CellBorder getLeftBorder() {
+        return leftBorder;
+    }
+
+    public void setLeftBorder(CellBorder leftBorder) {
+        this.leftBorder = leftBorder;
+    }
+
+    public CellBorder getRightBorder() {
+        return rightBorder;
+    }
+
+    public void setRightBorder(CellBorder rightBorder) {
+        this.rightBorder = rightBorder;
+    }
+
+    public CellVerticalAlignment getVerticalAlignment() {
+        return verticalAlignment;
+    }
+
+    public void setVerticalAlignment(CellVerticalAlignment verticalAlignment) {
+        this.verticalAlignment = verticalAlignment;
+    }
+
+    public CellHorizontalAlignment getHorizontalAlignment() {
+        return horizontalAlignment;
+    }
+
+    public void setHorizontalAlignment(CellHorizontalAlignment horizontalAlignment) {
+        this.horizontalAlignment = horizontalAlignment;
+    }
+
+    public CellFillPattern getFillPattern() {
+        return fillPattern;
+    }
+
+    public void setFillPattern(CellFillPattern fillPattern) {
+        this.fillPattern = fillPattern;
+    }
+
+    public XSSFColor getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(XSSFColor foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public XSSFColor getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(XSSFColor backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public boolean isWrappedText() {
+        return wrappedText;
+    }
+
+    public void setWrappedText(boolean wrappedText) {
+        this.wrappedText = wrappedText;
+    }
+
+    public short getIndentationInSpaces() {
+        return indentationInSpaces;
+    }
+
+    public void setIndentationInSpaces(short indentationInSpaces) {
+        this.indentationInSpaces = indentationInSpaces;
+    }
+
+    public XSSFCellStyle toXSSFCellStyle(XSSFWorkbook workbook) {
+        if (workbook != null) {
+            XSSFCellStyle xssfCellStyle = workbook.createCellStyle();
+            xssfCellStyle.setBorderTop(topBorder.toPoiCellStyle());
+            xssfCellStyle.setBorderBottom(bottomBorder.toPoiCellStyle());
+            xssfCellStyle.setBorderLeft(leftBorder.toPoiCellStyle());
+            xssfCellStyle.setBorderRight(rightBorder.toPoiCellStyle());
+            xssfCellStyle.setAlignment(horizontalAlignment.toPoiCellStyle());
+            xssfCellStyle.setVerticalAlignment(verticalAlignment.toPoiCellStyle());
+            xssfCellStyle.setFillForegroundColor(foregroundColor.getIndexed());
+            xssfCellStyle.setFillBackgroundColor(backgroundColor.getIndexed());
+            xssfCellStyle.setFillPattern(fillPattern.toPoiCellStyle());
+            xssfCellStyle.setWrapText(this.wrappedText);
+            xssfCellStyle.setIndention(this.indentationInSpaces);
+            return xssfCellStyle;
+        } else {
+            throw new IllegalArgumentException("Cannot create XSSFCellStyle using a null XSSFWorkbook!");
+        }
+    }
 }
