55cdbeeeb7bc1bed7919f2c529a9ca23543ed2c2
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2017-07-16 00:26:14 -0600

    Test stub for AbstractManifestController

---
+++ test/com/starrypenguin/rmcnew/controller/AbstractManifestControllerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Richard Scott McNew.
+ *
+ * This file is part of CRC Manifest Processor.
+ *
+ *     CRC Manifest Processor is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CRC Manifest Processor is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with CRC Manifest Processor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.starrypenguin.rmcnew.controller;
+
+import org.junit.Test;
+
+/**
+ * AbstractManifestControllerTest
+ * <p/>
+ * Tests for AbstractManifestController
+ */
+public class AbstractManifestControllerTest {
+
+    @Test
+    public void processManifestFileTest() {
+
+    }
+
+    @Test
+    public void processFinalDestinationTest() {
+
+    }
+
+    @Test
+    public void processHubLookupTest() {
+
+    }
+
+    @Test
+    public void applyBusinessRulesTest() {
+
+    }
+}
